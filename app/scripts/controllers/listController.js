6ab2c391da9d5338641bc04bba1a1617e1ffdb97
Daniel Atkinson <hourd.tasa@gmail.com>
2015-08-18 21:25:40 +0100

    added the ability to delete and item from the list

--- app/scripts/controllers/listController.js
+++ app/scripts/controllers/listController.js
@@ -1,3 +1,10 @@
 application.controller('ListController', function ($scope, list) {  
   $scope.list = list;
+  $scope.deleteItem = function(id) {
+    var newList = JSON.parse(JSON.stringify($scope.list.items));
+    if(id > -1) {
+      newList.splice(id, 1);
+    }
+    $scope.list.items = JSON.parse(JSON.stringify(newList));
+  };
 })
